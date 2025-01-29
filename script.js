8c0a1761eeb00246eb6e2da78736561d8625e00b
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-01-29 18:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//your JS code here. If required.
-const student = { name: "John" };
+const myObj = {
+    name: "John",
+    age: 25,
+    getKeys: function () {
+        return Object.keys(this);
+    }
+};
 
-function getKeys(obj) {
-    return Object.keys(obj);
-}
-
-console.log(getKeys(student));
+console.log(myObj.getKeys());
