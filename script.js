9cefc27e622769b5ac534aead023f55e692e3ef9
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-01 20:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const student = {
-  name: "John"
+  name: "John",
+  getKeys: function() {
+    return Object.keys(this);
+  }
 };
-
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-console.log(getKeys(student));
 
 // Do not change the code below this line
 window.student = student;
-window.getKeys = getKeys;
